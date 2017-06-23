7494c50b78cd459dcae3efb7bb2e9e61035d9356
Ryan Winchester <code@ryanwinchester.ca>
2017-06-23 10:45:28 -0700

    add :ssl application

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Navii.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :ssl],
      mod: {Navii.Application, []}]
   end
 
