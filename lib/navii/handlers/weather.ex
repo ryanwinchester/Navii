e7544c12b8d17e9bbf41a886bb0da71854c597c8
Ryan Winchester <code@ryanwinchester.ca>
2017-06-24 15:51:43 -0700

    Update weather crap

--- lib/navii/handlers/weather.ex
+++ lib/navii/handlers/weather.ex
@@ -8,7 +8,7 @@ defmodule Navii.Weather do
   end
 
   defmodule Weather do
-    defstruct [:geo, :currently, :hourly, :daily]
+    defstruct [:geo, :temperature, :humidity, :currently, :hourly, :daily]
   end
 
   use Kuma.Handler
@@ -25,6 +25,7 @@ defmodule Navii.Weather do
       |> get_geo()
       |> get_weather()
       |> format_weather()
+      |> replace_temps()
       |> Bot.send(channel)
     end
 
@@ -36,8 +37,9 @@ defmodule Navii.Weather do
 
   # Get the geographic info from Google Maps
   defp get_geo(nil), do: %Geo{}
+  defp get_geo([loc | _]), do: get_geo(loc)
   defp get_geo(loc) do
-    geo_url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=#{loc}"
+    geo_url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=#{URI.encode(loc)}"
 
     case HTTPoison.get(geo_url) do
       {:ok, %{status_code: 200, body: body}} ->
@@ -60,8 +62,11 @@ defmodule Navii.Weather do
     case HTTPoison.get(darksky_url) do
       {:ok, %{status_code: 200, body: body}} ->
         weather = Poison.decode!(body)
+        Logger.debug inspect(weather["currently"])
         %Weather{
           geo: geo,
+          temperature: weather["currently"]["temperature"],
+          humidity: weather["currently"]["humidity"],
           currently: weather["currently"]["summary"],
           hourly: weather["hourly"]["summary"],
           daily: weather["daily"]["summary"],
@@ -76,7 +81,17 @@ defmodule Navii.Weather do
   defp format_weather(%Weather{currently: nil}), do: "No idea..."
   defp format_weather(%Weather{} = weather) do
     """
-    Weather for #{weather.geo.text}: #{weather.currently}. #{weather.hourly} #{weather.daily}
+    Weather for #{weather.geo.text}: #{weather.currently}. #{weather.temperature}°F #{round(weather.humidity * 100)}% humidity. #{weather.hourly} #{weather.daily}
     """
+  end
+
+  # Replace temps in F in a string to C/F
+  defp replace_temps(str) do
+    Regex.replace(~r/(\d+(?:\.\d+)?) ?°F/, str, fn _, temp ->
+      {deg_f, _} = Float.parse(temp)
+      deg_c = ((deg_f - 32) * (5/9)) |> round()
+      deg_f = round(deg_f)
+      "#{deg_c}°C (#{deg_f}°F)"
+    end)
   end
 end
