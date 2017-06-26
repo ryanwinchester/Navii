308278f7653bff7d93ae0113ba15de5fdb6df1ba
Ryan Winchester <code@ryanwinchester.ca>
2017-06-25 22:07:01 -0700

    Currencies

--- lib/navii/responders/currency.ex
+++ lib/navii/responders/currency.ex
@@ -11,24 +11,31 @@ defmodule Navii.Responders.Currency do
   @currency_choices Enum.join(@currencies, "|")
 
   respond ~r/convert ?([0-9.]+) ?(#{@currency_choices}) (?:into|in|to)? ?(#{@currency_choices})/i, msg do
-    {_amount, _} = Float.parse(msg.matches[1])
+    {amount, _} = Float.parse(msg.matches[1])
     from = String.upcase(msg.matches[2])
     to = String.upcase(msg.matches[3])
 
-    _exchange = fetch_rates(from, to)
+    {from_rate, to_rate} = fetch_rates(from, to)
 
-    send msg, "TODO"
+    converted = ((amount / from_rate) * to_rate) |> Float.round(2)
+    amount = Float.round(amount, 2)
+
+    send msg, "#{amount} #{from} is #{converted} #{to}"
   end
 
-  defp fetch_rates(_from, _to) do
+  # Fetch the exchange rates
+  defp fetch_rates(from, to) do
     url = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
     headers = [{"User-Agent", "Mozilla/5.0"}]
 
     case HTTPoison.get(url, headers) do
       {:ok, %{status_code: 200, body: body}} ->
-        Logger.warn inspect(body)
+        [from_rate | _] = Regex.run(~r/currency='#{from}' rate='(.+)'/, body, capture: :all_but_first)
+        [to_rate | _] = Regex.run(~r/currency='#{to}' rate='(.+)'/, body, capture: :all_but_first)
+        {String.to_float(from_rate), String.to_float(to_rate)}
       {_, res} ->
         Logger.warn inspect(res)
+        :error
     end
   end
 end
