b587b0a75b832412d3e091fb72c810654c834464
Ryan Winchester <code@ryanwinchester.ca>
2017-12-04 12:32:58 -0800

    ommgggg

--- lib/navii/responders/eight_ball.ex
+++ lib/navii/responders/eight_ball.ex
@@ -34,7 +34,7 @@ defmodule Navii.Responders.EightBall do
     "Very doubtful",
   ]
 
-  respond ~r/.?.?will .*\?.?$/i, msg do
+  respond ~r/.?.?will .+\?/i, msg do
     reply msg, Enum.random(@responses)
   end
 end
